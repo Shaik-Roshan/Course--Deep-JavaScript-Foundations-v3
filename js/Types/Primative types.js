47419a7c717d38069f9d3669809579b99ba601a2
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-17 23:29:55 +0530

    Added

---
+++ js/Types/Primative types.js
@@ -0,0 +1,34 @@
+//undefined
+//String
+//number
+//boolean
+//object
+//symbol
+//mull
+// BigInt(future)
+
+// In JavaScript, variables dont have types. values do.
+//primative types stores actual values
+
+var a;
+console.log(a);
+console.log(typeof a); //undefined
+
+var b = 10;
+console.log(b);
+console.log(typeof b); //number
+
+var c = true;
+console.log(c);
+console.log(typeof c); //boolean
+
+var d = "Roshan";
+console.log(d);
+console.log(typeof d); //String
+
+var e = {};
+console.log(e);
+console.log(typeof d); //Object
+
+var f = Symbol();
+console.log(typeof f); //Symbol
