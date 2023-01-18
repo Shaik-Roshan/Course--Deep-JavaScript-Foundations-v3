59963d04f6ab3c7b4207efb3aa318f98be4ef963
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-18 22:43:30 +0530

    Added

--- js/Types/Primative types.js
+++ js/Types/Primative types.js
@@ -11,24 +11,40 @@
 //primative types stores actual values
 
 var a;
-console.log(a);
+console.log(a); //undefine
 console.log(typeof a); //undefined
 
 var b = 10;
-console.log(b);
+console.log(b); //10
 console.log(typeof b); //number
 
 var c = true;
-console.log(c);
+console.log(c); //true
 console.log(typeof c); //boolean
 
 var d = "Roshan";
-console.log(d);
+console.log(d); //Roshan
 console.log(typeof d); //String
 
 var e = {};
-console.log(e);
-console.log(typeof d); //Object
+console.log(e); //object
+console.log(typeof e); //Object
 
 var f = Symbol();
 console.log(typeof f); //Symbol
+
+
+// typeof doesntExit; //"undefined"
+
+var g=null;
+console.log(g);//"object"
+
+var h = [1, 2, 3, 4];
+console.log(typeof h); //object
+
+var i=function(){};
+console.log(typeof(i));//"function"
+ 
+
+
+
