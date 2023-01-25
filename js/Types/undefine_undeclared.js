45b85638d57f8d06af390deb2f1b3024884629dd
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-25 23:13:24 +0530

    Added

---
+++ js/Types/undefine_undeclared.js
@@ -0,0 +1,6 @@
+// in javascript undefine and undeclared is soo different as undefine means there is variable exits and no value assigned and in undeclared nothing is created
+
+var r;
+console.log(typeof r);//undefine
+
+// console.log(a);
